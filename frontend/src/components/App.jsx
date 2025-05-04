fbc36032d3503fdeaf00ca49360a7582a1d31f23
Wendy <cutebitterblue@gmail.com>
2025-05-04 13:56:59 +0800

    React Transition Group
    Add React Transition Group

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Route, Routes, BrowserRouter, useLocation} from "react-router-dom";
+import {CSSTransition, SwitchTransition} from 'react-transition-group';
 import "../../bootstrap/bootstrap.css";
 import "../../bootstrap/bootstrap.js";
 import "./style.css";
@@ -9,22 +10,39 @@ import Login from "./login/Login.jsx";
 import Profile from "./profile/Profile.jsx";
 import Write from "./write/Write.jsx";
 
+import './index.css';
+
 function App() {
   return (
     <Auth>
       <BrowserRouter>
         <div className="murmur">
           <Navbar/>
-          <Routes>
-            <Route path="/" element={<Feed/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-            <Route path="/write" element={<Write/>}/>
-          </Routes>
+          <RoutesLocation/>
         </div>
       </BrowserRouter>
     </Auth>
   );
 }
 
+function RoutesLocation() {
+  const location = useLocation();
+
+  return (
+    <SwitchTransition>
+      <CSSTransition key={location.pathname} classNames="forward" timeout={300}>
+        <div>
+          <Routes location={location}>
+            <Route path="/" element={<Feed/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/write" element={<Write/>}/>
+          </Routes>
+        </div>
+      </CSSTransition>
+    </SwitchTransition>
+  );
+}
+
 export default App;
+
